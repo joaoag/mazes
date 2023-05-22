5283425cc3dd47832774a2730baf4365e4c4ec67
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2023-05-22 21:08:11 +0100

    wip: further implementation of cell type

--- src/cell.rs
+++ src/cell.rs
@@ -1,9 +1,29 @@
+#[derive(Debug)]
 pub struct Cell {
     row: u8,
     column: u8,
-    north: Option<Cell>,
-    east: Option<Cell>,
-    south: Option<Cell>,
-    west: Option<Cell>,
+    north: Option<Box<Cell>>,
+    east: Option<Box<Cell>>,
+    south: Option<Box<Cell>>,
+    west: Option<Box<Cell>>,
 }
 
+fn main(){
+    let cell_one: Cell = Cell {
+        row: 1,
+        column: 1,
+        north:  None::<Box<Cell>>,
+        east: None::<Box<Cell>>,
+        south: None::<Box<Cell>>,
+        west: None::<Box<Cell>>
+    };
+    let cell_two: Cell = Cell{
+        row: 0,
+        column: 0,
+        north: Some(Box::new(cell_one)),
+        east: None::<Box<Cell>>,
+        south: None::<Box<Cell>>,
+        west: None::<Box<Cell>>
+    };
+    println!("{:#?}", cell_two);
+}

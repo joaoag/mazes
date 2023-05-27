e150f4aa726a5fa114ae42520bfa3903a542d1c4
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2023-05-27 22:32:09 +0100

    wip: starting to implement linking and unlinking cells

--- src/cell.rs
+++ src/cell.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub struct Cell {
     row: u8,
@@ -6,6 +8,32 @@ pub struct Cell {
     east: Option<Box<Cell>>,
     south: Option<Box<Cell>>,
     west: Option<Box<Cell>>,
+    links: HashMap<Cell, bool>
+}
+
+
+// these probably need to be implemented as traits - might be useful for a blog post?
+
+fn link(source: Cell, target: Cell){
+    source.links.insert(
+        target,
+        true
+    );
+    target.links.insert(
+        source,
+        true
+    )
+    // not sure what's going to happen with ownership here
+}
+
+fn unlink(source: Cell, target: Cell){
+    source.links.remove(
+        target
+    );
+    target.links.remove(
+        source
+    );
+    // same here - who owns what???
 }
 
 fn main(){
@@ -15,7 +43,8 @@ fn main(){
         north:  None::<Box<Cell>>,
         east: None::<Box<Cell>>,
         south: None::<Box<Cell>>,
-        west: None::<Box<Cell>>
+        west: None::<Box<Cell>>,
+        links: HashMap::new()
     };
     let cell_two: Cell = Cell{
         row: 0,
@@ -23,7 +52,13 @@ fn main(){
         north: Some(Box::new(cell_one)),
         east: None::<Box<Cell>>,
         south: None::<Box<Cell>>,
-        west: None::<Box<Cell>>
+        west: None::<Box<Cell>>,
+        links: HashMap::new()
     };
     println!("{:#?}", cell_two);
 }
+
+
+// next step, look into #[derive(Eq, Hash, PartialEq)]
+// why do you need to derive it?
+// why would it not be implemented by default?
